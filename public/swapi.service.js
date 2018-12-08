058dd878d5176365e0d5b2bb3d41ff84cc0234aa
Leslie Borst <leslieaborst@gmail.com>
2018-12-08 13:24:13 -0500

    more api calls

--- public/swapi.service.js
+++ public/swapi.service.js
@@ -6,14 +6,20 @@ angular
     const service = this;
     service.userAnswers = [];
 
-    service.getPeople = (people) => {
+    service.getPeople = (id) => {
       return $http({
         method: "GET",
-        url: "proxy/https://swapi.co/api/people",
-        data: people
+        url: `proxy/https://swapi.co/api/people/${id}`
       });
     };
 
+    service.getMovies = (list) => {
+      const promises = list.map(url => $http.get(`proxy/${url}`));
+      return Promise.all(promises);
+    };
 
+    service.getPlanets = (url) => {
+      return $http.get(`proxy/${url}`);
+    };
 
   });
